85c6d01747a7c575ca6913b211cf3734fc41531a
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-06-19 10:42:50 -0500

    Fixed wording

--- appointment.js
+++ appointment.js
@@ -1,3 +1,20 @@
 function scheduler() {
+    let patientName;
+    let doctor;
+    let time;
+    let date;
+    let message;
+
+    patientName = prompt("What is the patient's name?");
+    doctor = prompt("Would you like to see doctor Batman, Hulk or Superman? Enter one of those three.");
+    date = prompt("What date would you prefer?mm/dd/yy");
+    time = prompt("What time of day do you prefer?");
+    message = prompt("What is your reason for coming in to see the doctor?");
+
+    review.innerHTML = `Hi! ${patientName}. Your appointment with Dr. ${doctor} is scheduled for:`;
+    appointmentParagraph.innerHTML = `${date} at ${time} o'clock.`;
+    message.innerHTML = `Message to doctor:<br><br> ${message}.`
+
     
 }
+appointmentButton.onclick = scheduler;
